47737bd10d82caf19c2c8370312e48a75ed1db50
Saki Sato <sakiisato@gmail.com>
2014-11-15 17:44:36 -0500

    added facebook graph api in place of fql

--- fb-events/index.php
+++ fb-events/index.php
@@ -12,29 +12,41 @@ xmlns:fb="http://www.facebook.com/2008/fbml">
 //requiring FB PHP SDK
 require 'fb-sdk/src/facebook.php';
 
-//initializing keys
-// Hard Nips Website
-// App ID created 11/15/2014
-$facebook = new Facebook(array(
-	'appId'  => '1428096120773288',
-	'secret' => '2fd26674a216fa59e76fa759f3ddd708',
-	'cookie' => true // enable optional cookie support
-));
+// //initializing keys
+// // Hard Nips Website
+// // App ID created 11/15/2014
+// $facebook = new Facebook(array(
+//   'appId'  => '1428096120773288',
+//   'secret' => '2fd26674a216fa59e76fa759f3ddd708',
+//   'cookie' => true // enable optional cookie support
+// ));
+//
+// //query the events
+// //currently selecting name, start_time, end_time, location, description this time
+// //but there's other data that you can get from the event table (https://developers.facebook.com/docs/reference/fql/event/)
+// $fql    =   "SELECT eid, name, start_time, end_time, location, description
+//       FROM event WHERE eid IN ( SELECT eid FROM event_member WHERE uid = 204470146095 ) AND start_time >= now()
+//       ORDER BY start_time asc"; // add LIMIT 0,3"; to only display 3 events
+//
+// $param  =   array(
+// 'method'    => 'fql.query',
+// 'query'     => $fql,
+// 'callback'  => ''
+// );
+//
+// $fqlResult   =   $facebook->api($param);
 
-//query the events
-//currently selecting name, start_time, end_time, location, description this time
-//but there's other data that you can get from the event table (https://developers.facebook.com/docs/reference/fql/event/)
-$fql    =   "SELECT eid, name, start_time, end_time, location, description 
-			FROM event WHERE eid IN ( SELECT eid FROM event_member WHERE uid = 204470146095 ) AND start_time >= now()
-			ORDER BY start_time asc"; // add LIMIT 0,3"; to only display 3 events
-			
-$param  =   array(
-'method'    => 'fql.query',
-'query'     => $fql,
-'callback'  => ''
+/* make the API call */
+FB.api(
+    "/204470146095/events",
+    function (response) {
+      console.log(response);
+      if (response && !response.error) {
+        /* handle the result */
+      }
+    }
 );
 
-$fqlResult   =   $facebook->api($param);
 
 if(empty($fqlResult)) {
 	echo "<h3 class='no-dates'>Oh no, Nips do not have show at this time :(</h3> <p>Follow us <a href='http://www.tumblr.com/follow/hardnipsband' title='Follow Hard Nips on Tumblr' target='_blank'>here on Tumblr</a>, or <a href='http://www.facebook.com/pages/HARD-NIPS/204470146095' title='Hard Nips on Facebook' target='_blank'>Facebook</a> and <a href='http://www.twitter.com/HardNips_band' title='Hard Nips on Twitter' target='_blank'>Twitter</a> for updates.</p>";	
