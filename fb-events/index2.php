971c50d3e81848b2e78ba452939fee715822c444
Saki Sato <sakiisato@gmail.com>
2014-11-15 20:43:14 -0500

    testing sdk

--- fb-events/index2.php
+++ fb-events/index2.php
@@ -42,67 +42,71 @@ session_start();
  
 // init app with app id and secret
 FacebookSession::setDefaultApplication( '1428096120773288','2fd26674a216fa59e76fa759f3ddd708' );
- 
-// login helper with redirect_uri
-$helper = new FacebookRedirectLoginHelper( 'http://hardnipsbrooklyn.com/' );
- 
-// see if a existing session exists
-if ( isset( $_SESSION ) && isset( $_SESSION['fb_token'] ) ) {
-  // create new session from saved access_token
-  $session = new FacebookSession( $_SESSION['fb_token'] );
-  
-  // validate the access_token to make sure it's still valid
-  try {
-    if ( !$session->validate() ) {
-      $session = null;
-    }
-  } catch ( Exception $e ) {
-    // catch any exceptions
-    $session = null;
-  }
-}  
- 
-if ( !isset( $session ) || $session === null ) {
-  // no session exists
-  
-  try {
-    $session = $helper->getSessionFromRedirect();
-  } catch( FacebookRequestException $ex ) {
-    // When Facebook returns an error
-    // handle this better in production code
-    print_r( $ex );
-  } catch( Exception $ex ) {
-    // When validation fails or other local issues
-    // handle this better in production code
-    print_r( $ex );
-  }
-  
-}
- 
-// see if we have a session
-if ( isset( $session ) ) {
-  
-  // save the session
-  $_SESSION['fb_token'] = $session->getToken();
-  // create a session using saved token or the new one we generated at login
-  $session = new FacebookSession( $session->getToken() );
-  
-  // graph api request for user data
-  $request = new FacebookRequest( $session, 'GET', '/me' );
-  $response = $request->execute();
-  // get response
-  $graphObject = $response->getGraphObject()->asArray();
-  
-  // print profile data
-  echo '<pre>' . print_r( $graphObject, 1 ) . '</pre>';
-  
-  // print logout url using session and redirect_uri (logout.php page should destroy the session)
-  echo '<a href="' . $helper->getLogoutUrl( $session, 'http://yourwebsite.com/app/logout.php' ) . '">Logout</a>';
-  
-} else {
-  // show login url
-  echo '<a href="' . $helper->getLoginUrl( array( 'email', 'user_friends' ) ) . '">Login</a>';
-}
+
+// If you already have a valid access token:
+// HardNips access token
+$session = new FacebookSession('CAACEdEose0cBANwYRr3uVzolZC50UJMkI8aHxrl3IwOmZC3F3eS3QZCR4LxqYyHuTeM8KJJDcZB4LiSnlDQZC45IKFI1fdadXO7JH8hyLnxuvfflfoq13ZB55fVvTgXHlo6MvBzw1c2gurZBLV52HrqH4ZA5ZAZAshhuA37mZCDso6EwoAZCZCB8xZCEK9ZA3cQMX7n0n1yGIZCVv8wxMtfFvyV8TI4O');
+
+// // login helper with redirect_uri
+// $helper = new FacebookRedirectLoginHelper( 'http://hardnipsbrooklyn.com/' );
+//
+// // see if a existing session exists
+// if ( isset( $_SESSION ) && isset( $_SESSION['fb_token'] ) ) {
+//   // create new session from saved access_token
+//   $session = new FacebookSession( $_SESSION['fb_token'] );
+//
+//   // validate the access_token to make sure it's still valid
+//   try {
+//     if ( !$session->validate() ) {
+//       $session = null;
+//     }
+//   } catch ( Exception $e ) {
+//     // catch any exceptions
+//     $session = null;
+//   }
+// }
+//
+// if ( !isset( $session ) || $session === null ) {
+//   // no session exists
+//
+//   try {
+//     $session = $helper->getSessionFromRedirect();
+//   } catch( FacebookRequestException $ex ) {
+//     // When Facebook returns an error
+//     // handle this better in production code
+//     print_r( $ex );
+//   } catch( Exception $ex ) {
+//     // When validation fails or other local issues
+//     // handle this better in production code
+//     print_r( $ex );
+//   }
+//
+// }
+//
+// // see if we have a session
+// if ( isset( $session ) ) {
+//
+//   // save the session
+//   $_SESSION['fb_token'] = $session->getToken();
+//   // create a session using saved token or the new one we generated at login
+//   $session = new FacebookSession( $session->getToken() );
+//
+//   // graph api request for user data
+//   $request = new FacebookRequest( $session, 'GET', '/me' );
+//   $response = $request->execute();
+//   // get response
+//   $graphObject = $response->getGraphObject()->asArray();
+//
+//   // print profile data
+//   echo '<pre>' . print_r( $graphObject, 1 ) . '</pre>';
+//
+//   // print logout url using session and redirect_uri (logout.php page should destroy the session)
+//   echo '<a href="' . $helper->getLogoutUrl( $session, 'http://yourwebsite.com/app/logout.php' ) . '">Logout</a>';
+//
+// } else {
+//   // show login url
+//   echo '<a href="' . $helper->getLoginUrl( array( 'email', 'user_friends' ) ) . '">Login</a>';
+// }
 
 // // Using SDK
 // // use Facebook\FacebookRequest;
